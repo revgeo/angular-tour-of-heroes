e67d49b98a85104dc778406737235ad412e4c54f
Geoff Ramsey <gramsey@dontpaniclabs.com>
2022-10-26 15:03:07 -0500

    added heroes , other stuff

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Mice' },
+    { id: 13, name: 'Mr. Rats' },
+    { id: 14, name: 'Miss Demeanor' },
+    { id: 15, name: 'A. Salt' },
+    { id: 16, name: '"Bat" Terry' },
+    { id: 17, name: 'Agent Sparkle' },
+    { id: 18, name: 'El Machismo' },
+    { id: 19, name: 'Hero McHeroface' },
+    { id: 20, name: 'Harold' },
+]
