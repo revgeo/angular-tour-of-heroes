11db53371f7cc9a3aee36839aa04d55071825fd2
Geoff Ramsey <gramsey@dontpaniclabs.com>
2022-10-27 10:31:46 -0500

    messages added?

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
   
-  constructor() { }
-
   ngOnInit(): void {
+    this.getHeroes();
   }
+
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
